7ee0f6911b8ecf302254a713cccde5e2a0e1b684
jungeun <jungeun.lee@teamsparta.co>
2024-01-01 00:05:43 +0900

    feat: 구글 소셜 로그인 구현

--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,21 @@
 import CreateDOM from 'react-dom/client';
 import React from 'react';
-import App from "./App";
+import App from './App';
+import {GoogleOAuthProvider} from '@react-oauth/google';
 
 const rootElement = document.getElementById('root');
 if (!rootElement) throw new Error('Failed to find the root element');
 const root = CreateDOM.createRoot(rootElement);
+const clientId = process.env.REACT_APP_GOOGLE_CLIENT_ID;
+
+if (!clientId) {
+	throw new Error('REACT_APP_GOOGLE_CLIENT_ID 환경 변수가 설정되지 않았습니다.');
+}
+
 root.render(
-  <React.StrictMode>
-    <App/>
-  </React.StrictMode>
+	<React.StrictMode>
+		<GoogleOAuthProvider clientId={clientId}>
+			<App />
+		</GoogleOAuthProvider>
+	</React.StrictMode>,
 );
--- src/pages/LoginPage/LoginPage.tsx
+++ src/pages/LoginPage/LoginPage.tsx
@@ -1,11 +1,20 @@
 import * as S from './LoginPage.style';
 import {GoogleLoginLogo} from '../../assets/svgs/login';
+import {useGoogleLogin} from '@react-oauth/google';
+import {useAtom} from 'jotai';
+import {isUserLoggedInAtom} from '../../stores/googleLogin';
 
 export const LoginPage = () => {
+	const [, setIsUserLoggedIn] = useAtom(isUserLoggedInAtom);
+	const login = useGoogleLogin({
+		onSuccess: tokenResponse => tokenResponse.access_token && setIsUserLoggedIn(true),
+		onError: () => console.log('로그인 실패'),
+	});
+
 	return (
 		<S.LoginPageWrapper>
 			<S.MoaPickLogo>모아픽 로고</S.MoaPickLogo>
-			<S.LoginButtonContainer>
+			<S.LoginButtonContainer onClick={() => login()}>
 				<GoogleLoginLogo /> Google 계정으로 로그인
 			</S.LoginButtonContainer>
 		</S.LoginPageWrapper>
