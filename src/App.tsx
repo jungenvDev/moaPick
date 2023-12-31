2af1a87381e5c0497c906452ef04fc3afdd4fa6f
jungeun <jungeun.lee@teamsparta.co>
2023-12-31 23:07:07 +0900

    feat: global style 적용, prettier 적용

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,24 @@
 import React from 'react';
 
-import { LoginPage } from "./pages/LoginPage/LoginPage";
-import { BrowserRouter as Router, Navigate, Route, Routes } from "react-router-dom";
-import { DashboardPage } from "./pages/DashboardPage/DashboardPage";
+import {LoginPage} from './pages/LoginPage/LoginPage';
+import {BrowserRouter as Router, Navigate, Route, Routes} from 'react-router-dom';
+import {DashboardPage} from './pages/DashboardPage/DashboardPage';
+import {GlobalStyle} from './globalStyle';
 
-const isAuthenticated = true; // 로그인 여부에 따라 변경
+const isAuthenticated = false; // 로그인 여부에 따라 변경
 
 function App() {
-    return (
-        <Router>
-            <Routes>
-                <Route path="/" element={isAuthenticated ? <DashboardPage /> : <Navigate to="/login" />} />
-                <Route path="/login" element={<LoginPage />} />
-            </Routes>
-        </Router>
-    );
+	return (
+		<>
+			<GlobalStyle />
+			<Router>
+				<Routes>
+					<Route path='/' element={isAuthenticated ? <DashboardPage /> : <Navigate to='/login' />} />
+					<Route path='/login' element={<LoginPage />} />
+				</Routes>
+			</Router>
+		</>
+	);
 }
 
 export default App;
