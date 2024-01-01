48345361c44cbb363dfbcb532855a470354fc291
jungeun <jungeun.lee@teamsparta.co>
2024-01-01 21:56:33 +0900

    feat: 플로팅 버튼 클릭시 모달 띄우기

--- src/components/atoms/DashboardPage/FloatingButton/FloatingButton.tsx
+++ src/components/atoms/DashboardPage/FloatingButton/FloatingButton.tsx
@@ -1,9 +1,16 @@
 import {FaPlus} from 'react-icons/fa';
 import * as S from './FloatingButton.style';
+import {useAtom} from 'jotai';
+import {isPostModalOpenAtom} from '../../../../stores/postModalOpen';
 
 export const FloatingButton = () => {
+	const [, setIsModalOpen] = useAtom(isPostModalOpenAtom);
 	return (
-		<S.FloatingButtonWrapper>
+		<S.FloatingButtonWrapper
+			onClick={() => {
+				setIsModalOpen(true);
+			}}
+		>
 			<FaPlus />
 		</S.FloatingButtonWrapper>
 	);
